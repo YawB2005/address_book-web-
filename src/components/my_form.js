81fe86cd4feca968fbe9a61a9362dd089a2d3335
barimahyaw <barima234@gmail.com>
2024-10-18 12:42:36 +0000

    new_one

--- src/components/my_form.js
+++ src/components/my_form.js
@@ -36,7 +36,7 @@ const MyForm = ({topic, submit, name, setName, phone, setPhone, e}) => {
           <div className="flex justify-center">
             <button
               type="submit"
-              className="bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700"
+              className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700"
             >
               Done
             </button>
